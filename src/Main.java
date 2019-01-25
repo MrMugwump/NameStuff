90082cd091bb646abb152c789c625947190facf3
alessandro ronchi <aronchi1257@apps4pps.net>
2019-01-25 11:32:52 -0800

    alessandro

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,15 @@ public class Main {
     private Main(){
         printBen();
         printSpencer();
+        printAle();
     }
     private void printBen(){
         System.out.println("My name is Ben Bridenbaugh");
     }
     private void printSpencer(){
         System.out.println("I'm Spencer Barton");
+    }
+    private void printAle() {
+        System.out.println("My name is Alessandro Ronchi");
     }
 }
