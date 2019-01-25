c09ad3bb6700e15d51e918fdfd50d0b93814c89b
MrMugwump <42780701+MrMugwump@users.noreply.github.com>
2019-01-25 11:22:22 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+    private Main(){
+        printBen();
+        printSpencer();
+    }
+    private void printBen(){
+        System.out.println("My name is Ben Bridenbaugh");
+    }
+    private void printSpencer(){
+        System.out.println("I'm Spencer Barton");
+    }
+}
